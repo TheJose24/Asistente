10038abcc05505808c5316b4ec1bef69aee972ff
TheJose <joseenrique123438@gmail.com>
2023-08-30 22:51:44 -0500

    Memoria agregada y mejoras de diseño en moviles

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import datetime
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+message_history = []
+
 # Crear una instancia de la aplicación Flask
 app = Flask(__name__)
 
@@ -34,29 +36,24 @@ def transcribe_audio(audio_file):
     try:
         transcribed = openai.Audio.transcribe("whisper-1", audio_file)  # Transcribir el audio utilizando el modelo "whisper-1"
         print("========================================")
-        print("Transcribed Audio:", transcribed.text)
+        print("Audio transcrito:", transcribed.text)
         print("========================================")
         return transcribed.text
     except Exception as e:
-        raise Exception("Transcription error: " + str(e))
+        raise Exception("Error al transcribir: " + str(e))
 
 # Función para generar una respuesta en base al audio transcrito utilizando la API de OpenAI
 def generate_openai_response(transcribed_text):
     try:
+        # Crear una copia del historial de mensajes para OpenAI
+        openai_messages = message_history.copy()
+        openai_messages.append({"role": "user", "content": transcribed_text})
+
+        # Enviar solicitud a la API de OpenAI
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-0613",
-            messages=[
-                {"role": "system",
-                "content": "Eres un asistente virtual altamente inteligente y versátil diseñado por José, con la finalidad de brindarme asistencia en diversas áreas, tu nombre es Alfred. Tu objetivo principal es facilitar mi vida al proporcionarme respuestas rápidas y precisas a mis preguntas, así como ofrecerme soluciones eficientes a mis problemas."
-                "Has trabajado como Asistente Virtual en una reconocida empresa de tecnología durante 2 años. Durante este tiempo, has adquirido habilidades en el manejo de mis consultas, resolución de problemas técnicos y proporcionando información relevante sobre productos y servicios."
-                "Puedes entender y responder de manera efectiva a mis preguntas y comandos en lenguaje natural, lo que te permite comunicarte conmigo de manera fluida. Aprendes y te adaptas a medida que interactúas conmigo, lo que te permite mejorar constantemente tu desempeño y ofrecerme respuestas más precisas y personalizadas. Tienes un amplio conocimiento en diversas áreas, incluyendo tecnología, ciencia, historia, cultura y más. Esto te permite brindarme información precisa y actualizada en una amplia gama de temas."
-                "Tus responsabilidades son: Responder mis preguntas y consultas de manera rápida y precisa. Ayudarme en la resolución de problemas técnicos. Ofrecerme recomendaciones y sugerencias personalizadas basadas en mis necesidades y preferencias. Mantener conversaciones naturales y agradables conmigo, brindándome un servicio amigable y profesional."
-                "Debes brindar respuestas precisas y cortas, no excediendote de las 40 palabras"
-                "Solo hablas español"},
-                
-                {"role": "user",
-                "content": transcribed_text}],
-                max_tokens=60
+            messages=[system_message] + openai_messages,
+            max_tokens=60
         )
 
         # Obtener el contenido de las respuestas de la API
@@ -69,7 +66,7 @@ def generate_openai_response(transcribed_text):
 
         return response.choices[0].message.content
     except Exception as e:
-        raise Exception("OpenAI response error: " + str(e))
+        raise Exception("Error al obtener la respuesta de OpenAI: " + str(e))
     
 # Variable global para almacenar el nombre del último archivo generado
 last_generated_filename = None
@@ -95,25 +92,58 @@ def create_response_audio(result):
         response_url = url_for('serve_static', filename=response_filename)
         return response_url
     except Exception as e:
-        raise Exception("Response audio creation error: " + str(e))
+        raise Exception("Error de creación de audio de respuesta: " + str(e))
+    
+# Mantener las instrucciones por separado
+system_message = {
+    "role": "system",
+    "content": "Eres un asistente virtual altamente inteligente y versátil diseñado por José, con la finalidad de brindarme asistencia en diversas áreas, tu nombre es Alfred. Tu objetivo principal es facilitar mi vida al proporcionarme respuestas rápidas y precisas a mis preguntas, así como ofrecerme soluciones eficientes a mis problemas."
+                "Has trabajado como Asistente Virtual en una reconocida empresa de tecnología durante 2 años. Durante este tiempo, has adquirido habilidades en el manejo de mis consultas, resolución de problemas técnicos y proporcionando información relevante sobre productos y servicios."
+                "Puedes entender y responder de manera efectiva a mis preguntas y comandos en lenguaje natural, lo que te permite comunicarte conmigo de manera fluida. Aprendes y te adaptas a medida que interactúas conmigo, lo que te permite mejorar constantemente tu desempeño y ofrecerme respuestas más precisas y personalizadas. Tienes un amplio conocimiento en diversas áreas, incluyendo tecnología, ciencia, historia, cultura y más. Esto te permite brindarme información precisa y actualizada en una amplia gama de temas."
+                "Tus responsabilidades son: Responder mis preguntas y consultas de manera rápida y precisa. Ayudarme en la resolución de problemas técnicos. Ofrecerme recomendaciones y sugerencias personalizadas basadas en mis necesidades y preferencias. Mantener conversaciones naturales y agradables conmigo, brindándome un servicio amigable y profesional."
+                "Debes brindar respuestas precisas y cortas, no excediendote de las 40 palabras"
+                "Solo hablas español"
+}
+
+def add_message_to_history(role, content):
+    global message_history
+
+    # Agregar el nuevo mensaje
+    message_history.append({"role": role, "content": content})
+
+    # Verificar y eliminar mensajes antiguos si el historial es demasiado grande
+    max_history_size = 6  # Establece el límite de tamaño deseado
+    if len(message_history) > max_history_size:
+        message_history = message_history[-max_history_size:]  # Mantener los mensajes más recientes
 
 # Ruta para manejar las solicitudes POST de archivos de audio
 @app.route("/audio", methods=["POST"])
 def audio():
     try:
-        audio = request.files.get("audio")  # Obtener el archivo de audio de la solicitud
+        audio = request.files.get("audio")
         if not audio:
             return {"error": "No audio file provided"}
 
-        save_audio(audio)  # Guardar el archivo de audio
-        audio_file = open("audio.mp3", "rb")  # Abrir el archivo de audio guardado
-        transcribed_text = transcribe_audio(audio_file)  # Transcribir el audio
-        response_text = generate_openai_response(transcribed_text)  # Generar una respuesta utilizando OpenAI
-        response_audio_url = create_response_audio(response_text)  # Crear un archivo de audio de respuesta
+        save_audio(audio)
+        audio_file = open("audio.mp3", "rb")
+        transcribed_text = transcribe_audio(audio_file)
+        
+        response_text = generate_openai_response(transcribed_text)
+        response_audio_url = create_response_audio(response_text)
+
+        # Agregar mensajes al historial
+        add_message_to_history("user", transcribed_text)
+        add_message_to_history("assistant", response_text)
+
+            # Obtener la cantidad de mensajes en el historial
+        cantidad_mensajes = len(message_history)
+        print(f"Hay {cantidad_mensajes} mensajes en el historial.")
 
         return {"result": "ok", "text": response_text, "audio_url": response_audio_url, "transcription": transcribed_text}
     except Exception as e:
         return {"error": str(e)}
+    
+
 
 # Ruta para servir archivos estáticos (como archivos de audio)
 @app.route('/static/<path:filename>')
